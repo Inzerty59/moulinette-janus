10496dfe2d0b0809051de615e758e182dc1bddd1
Inzerty <dev.inzerty@groupevitaminet.com>
2025-09-17 17:05:31 +0200

    feat: add crud user

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -13,7 +13,7 @@ class DashboardController extends AbstractDashboardController
 {
     public function index(): Response
     {
-        return parent::index();
+        return $this->render('@EasyAdmin/page/content.html.twig');
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
@@ -45,6 +45,6 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-users', \App\Entity\User::class);
     }
 }
